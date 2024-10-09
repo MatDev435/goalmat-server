fbf8bc2cc522dfa44f3a9865f953aed59f52c05d
MatDev435 <matheuspsalmazo2@gmail.com>
2024-10-09 10:35:58 -0300

    Use cases: Delete goal

---
+++ src/use-cases/goals/delete-goal.spec.ts
@@ -0,0 +1,70 @@
+import { makeUser } from '../../../test/factories/make-user'
+import { InMemoryUsersRepository } from '../../../test/repositories/in-memory-users-repository'
+import { InMemoryGoalsRepository } from '../../../test/repositories/in-memory-goals-repository'
+import { DeleteGoalUseCase } from './delete-goal'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { makeGoal } from '../../../test/factories/make-goal'
+import { NotAllowedError } from '../errors/not-allowed-error'
+
+let inMemoryGoalsRepository: InMemoryGoalsRepository
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: DeleteGoalUseCase
+
+describe('Delete Goal Use Case', () => {
+  beforeEach(() => {
+    inMemoryGoalsRepository = new InMemoryGoalsRepository()
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new DeleteGoalUseCase(
+      inMemoryGoalsRepository,
+      inMemoryUsersRepository
+    )
+
+    inMemoryUsersRepository.items.push(
+      makeUser({
+        id: 'user-01',
+      })
+    )
+  })
+
+  it('should be able to delete a goal', async () => {
+    inMemoryGoalsRepository.items.push(
+      makeGoal({
+        id: 'goal-01',
+        ownerId: 'user-01',
+      })
+    )
+
+    const { success } = await sut.execute({
+      userId: 'user-01',
+      goalId: 'goal-01',
+    })
+
+    expect(success).toBe(true)
+    expect(inMemoryGoalsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a goal with an inexistent user', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'inexistent-user',
+        goalId: 'goal-01',
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to delete a goal from other user', async () => {
+    inMemoryGoalsRepository.items.push(
+      makeGoal({
+        id: 'goal-01',
+        ownerId: 'user-02',
+      })
+    )
+
+    await expect(() =>
+      sut.execute({
+        userId: 'user-01',
+        goalId: 'goal-01',
+      })
+    ).rejects.toBeInstanceOf(NotAllowedError)
+  })
+})
---
+++ src/use-cases/goals/delete-goal.ts
@@ -0,0 +1,45 @@
+import { UsersRepository } from '../../repositories/users-repository'
+import { GoalsRepository } from '../../repositories/goals-repository'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { NotAllowedError } from '../errors/not-allowed-error'
+
+interface DeleteGoalUseCaseRequest {
+  userId: string
+  goalId: string
+}
+
+interface DeleteGoalUseCaseResponse {
+  success: boolean
+}
+
+export class DeleteGoalUseCase {
+  constructor(
+    private goalsRepository: GoalsRepository,
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({
+    userId,
+    goalId,
+  }: DeleteGoalUseCaseRequest): Promise<DeleteGoalUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    const goal = await this.goalsRepository.findById(goalId)
+
+    if (!goal) {
+      throw new ResourceNotFoundError()
+    }
+
+    if (userId !== goal.ownerId) {
+      throw new NotAllowedError()
+    }
+
+    await this.goalsRepository.delete(goal.id)
+
+    return { success: true }
+  }
+}
